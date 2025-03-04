bdaff98ac266564638881001b1b2b7920f8abd74
Braian Lopez <braiilopez16@gmail.com>
2025-03-04 18:21:01 -0300

    no message

---
+++ Models/Log.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleAppBuenasPracticas1.Entities;
+
+namespace ConsoleAppBuenasPracticas1.Models
+{
+    public class Log
+    {
+        public static void Escribir(string usuario, string tipo, string descripcion)
+        {
+            try
+            {
+                LOGDBEntities db = new LOGDBEntities();
+                AppLog appLog = new AppLog();
+                appLog.Usuario = usuario;
+                appLog.Tipo = tipo;
+                appLog.Descripcion = descripcion;
+                db.AppLogs.Add(appLog);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter("c:\\LogApp.txt"))
+                    {
+                        sw.WriteLine(ex.Message);
+                        sw.WriteLine(ex.InnerException);
+
+                        sw.WriteLine(ex.StackTrace);
+                        sw.WriteLine(ex.Source);
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ConsoleAppBuenasPracticas1.Entities;
+using ConsoleAppBuenasPracticas1.Models;
 
 namespace ConsoleAppBuenasPracticas1
 {
@@ -15,25 +16,25 @@ namespace ConsoleAppBuenasPracticas1
             LOGDBEntities db = new LOGDBEntities();
             try
             {
+                Log.Escribir("1","INFO","instancié el articulo");
                 Articulo articuloAAgregarALaBaseDeDatos = new Articulo();
-                articuloAAgregarALaBaseDeDatos.Nombre = "Curacocos";
+                articuloAAgregarALaBaseDeDatos.Nombre = "Curacocos de bolsillo";
                 articuloAAgregarALaBaseDeDatos.Descripcion = "Frasco optimizado para curar flores de cannabis";
                 articuloAAgregarALaBaseDeDatos.Precio = 45000;
                 //agrego el articulo
+                Log.Escribir("1", "INFO", "Cargué el articulo en la bd");
+
                 db.Articulos.Add(articuloAAgregarALaBaseDeDatos);
                 //guardo en la base de datos
+                Log.Escribir("1", "INFO", "guardé los cambios");
                 db.SaveChanges();
                 throw new Exception("excepcion personalizada");
             }
             catch (Exception ex)
             {
-                AppLog appLog = new AppLog();
-                appLog.Usuario = "1";
-                appLog.Tipo = "EXCEPCION";
-                appLog.Descripcion = "Error creando la conexion a la base de datos: " + ex.Message.ToString();
-                db.AppLogs.Add(appLog);
-                db.SaveChanges();
-                //Console.WriteLine("Error creando la conexion a la base de datos: " + ex.Message.ToString());
+                Log.Escribir("1","EXCEPCION","Error guardando el articulo en la base de datos: "+ex.Message.ToString());
+                Console.WriteLine("No se encontró la ruta en la que quieres crear el archivo: " + ex.Message.ToString());
+
             }
             Console.Read();
             //try
